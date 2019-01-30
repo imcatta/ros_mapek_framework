9822b16fa081941e3f875228919f7cfb89d47374
Andrea Cattaneo <38829883+imcatta@users.noreply.github.com>
2019-01-30 16:09:17 +0100

    Add reference section in documentation (#10)
    * Removed unused files
    
    * Add docstrings
    
    * Add reference section in documentation

--- mapek_framework/src/mapek_framework/managed_system.py
+++ mapek_framework/src/mapek_framework/managed_system.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+"""
+managed_system.py
+-----------------
+"""
+from abc import ABCMeta
 
 class ManagedSystem(object):
-    pass
+    """Abstract class supposed to be extended to implement a particular managed system."""
+    
+    __metaclass__ = ABCMeta
     
    
--- mapek_framework/src/mapek_framework/ticker.py
+++ mapek_framework/src/mapek_framework/ticker.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
+"""
+ticker.py
+---------
+"""
 import rospy
 from std_msgs.msg import Empty
 
+
 class Ticker(object):
+    """
+    A simple object that publish 'tick' messages on a ROS topic at a given rate.
+
+    Args:
+        node_name (str): The desired ROS node name.
+        topic (str): The topic on which to send the ticks.
+        param3 (double): The rate of tick's pubblication.
+    """
 
     def __init__(self, node_name, topic, rate):
         self._node_name = node_name
-        self._topic = topic 
+        self._topic = topic
         self._rate = rate
 
     def spin(self):
+        """
+        Start the ticker.
+        """
         rospy.init_node(self._node_name)
         pub = rospy.Publisher(self._topic, Empty, queue_size=10)
         rate = rospy.Rate(self._rate)
