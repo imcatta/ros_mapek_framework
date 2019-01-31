bd13cb7ba788c20f5a507d74636e24121bea7534
Andrea Cattaneo <andreacattaneo96@gmail.com>
2019-01-31 17:43:01 +0100

    ManagedSystem TestCase

--- mapek_framework/src/test_mapek_framework/run_tests.py
+++ mapek_framework/src/test_mapek_framework/run_tests.py
@@ -3,6 +3,7 @@ import unittest
 import rosunit
 from .test_group import GroupTestCase
 from .test_interaction import InteractionTestCase
+from .test_managed_system import ManagedSystemTestCase
 
 
 if __name__ == '__main__':
@@ -10,3 +11,5 @@ if __name__ == '__main__':
                     'test_group_mapek_framework', GroupTestCase)
     rosunit.unitrun('test_mapek_framework',
                     'test_interaction_mapek_framework', InteractionTestCase)
+    rosunit.unitrun('test_mapek_framework',
+                    'test_managed_system_mapek_framework', ManagedSystemTestCase)
