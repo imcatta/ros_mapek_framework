40ee17a8a68c99488575bb94e7330c79827e001a
Andrea Cattaneo <andreacattaneo96@gmail.com>
2019-02-03 14:45:00 +0100

    Add branch param to codecov report

--- coverage.sh
+++ coverage.sh
@@ -3,4 +3,4 @@ pip install codecov nose
 cd /tmp
 nosetests --cover-package=mapek_framework --with-coverage --cover-xml --cover-xml-file /tmp/a.xml $TARGET_REPO_PATH/mapek_framework/src
 cd $TARGET_REPO_PATH
-codecov -f /tmp/a.xml -t $CODECOV_TOKEN
+codecov -f /tmp/a.xml -t $CODECOV_TOKEN -b $TRAVIS_BRANCH
